ac882e354d78e7d1259428116668b7440d726440
Rijan Regmi <rijanregmi111@gmail.com>
2025-12-17 19:56:49 +0545

    create bottom screen

---
+++ lib/screens/bottom_screen/note_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(child: Text('Welcome to the Note screen')),
+    );
+  }
+}
