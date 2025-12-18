2a046d1aea5b333ce4d84b98d839be83413e43d3
Rijan Regmi <rijanregmi111@gmail.com>
2025-12-18 22:40:56 +0545

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+import "package:hamropadhai/app.dart";
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-    );
-  }
+  runApp(const App());
 }
